d35eb4371a9c26cda643ae860fd60897d4458fd9
Steffen Rost <srost@de.ibm.com>
2020-07-07 17:42:31 +0000

    retry on all test failures (#43)

--- tests/src/test/scala/org/apache/openwhisk/core/controller/test/BasicAuthenticateTests.scala
+++ tests/src/test/scala/org/apache/openwhisk/core/controller/test/BasicAuthenticateTests.scala
@@ -19,6 +19,8 @@ package org.apache.openwhisk.core.controller.test
 
 import scala.concurrent.Await
 
+import scala.concurrent.duration.DurationInt
+
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
@@ -42,121 +44,181 @@ import org.apache.openwhisk.core.entitlement.Privilege
  */
 @RunWith(classOf[JUnitRunner])
 class BasicAuthenticateTests extends ControllerTestCommon {
+  private val retriesOnTestFailures = 5
+  private val waitBeforeRetry = 1.second
+
   behavior of "Authenticate"
 
   it should "authorize a known user using different namespaces and cache key, and reject invalid secret" in {
-    implicit val tid = transid()
-    val subject = Subject()
+    org.apache.openwhisk.utils
+      .retry(
+        {
+          implicit val tid = transid()
+          val subject = Subject()
 
-    val uuid1 = UUID()
-    val uuid2 = UUID()
+          val uuid1 = UUID()
+          val uuid2 = UUID()
 
-    val namespaces = Set(
-      WhiskNamespace(
-        Namespace(MakeName.next("authenticatev_tests"), uuid1),
-        BasicAuthenticationAuthKey(uuid1, Secret())),
-      WhiskNamespace(
-        Namespace(MakeName.next("authenticatev_tests"), uuid2),
-        BasicAuthenticationAuthKey(uuid2, Secret())))
-    val entry = WhiskAuth(subject, namespaces)
-    put(authStore, entry) // this test entry is reclaimed when the test completes
+          val namespaces = Set(
+            WhiskNamespace(
+              Namespace(MakeName.next("authenticatev_tests"), uuid1),
+              BasicAuthenticationAuthKey(uuid1, Secret())),
+            WhiskNamespace(
+              Namespace(MakeName.next("authenticatev_tests"), uuid2),
+              BasicAuthenticationAuthKey(uuid2, Secret())))
+          val entry = WhiskAuth(subject, namespaces)
+          put(authStore, entry) // this test entry is reclaimed when the test completes
 
-    // Try to login with each specific namespace
-    namespaces.foreach { ns =>
-      withClue(s"Trying to login to $ns") {
-        waitOnView(authStore, ns.authkey, 1) // wait for the view to be updated
-        val pass = BasicHttpCredentials(ns.authkey.uuid.asString, ns.authkey.key.asString)
-        val user = Await.result(
-          BasicAuthenticationDirective
-            .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
-          dbOpTimeout)
-        user.get shouldBe Identity(subject, ns.namespace, ns.authkey, rights = Privilege.ALL)
+          // Try to login with each specific namespace
+          namespaces.foreach {
+            ns =>
+              withClue(s"Trying to login to $ns") {
+                waitOnView(authStore, ns.authkey, 1) // wait for the view to be updated
+                val pass = BasicHttpCredentials(ns.authkey.uuid.asString, ns.authkey.key.asString)
+                val user = Await.result(
+                  BasicAuthenticationDirective
+                    .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
+                  dbOpTimeout)
+                user.get shouldBe Identity(subject, ns.namespace, ns.authkey, rights = Privilege.ALL)
 
-        // first lookup should have been from datastore
-        stream.toString should include(s"serving from datastore: ${CacheKey(ns.authkey)}")
-        stream.reset()
+                // first lookup should have been from datastore
+                stream.toString should include(s"serving from datastore: ${CacheKey(ns.authkey)}")
+                stream.reset()
 
-        // repeat query, now should be served from cache
-        val cachedUser = Await.result(
-          BasicAuthenticationDirective
-            .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
-          dbOpTimeout)
-        cachedUser.get shouldBe Identity(subject, ns.namespace, ns.authkey, rights = Privilege.ALL)
+                // repeat query, now should be served from cache
+                val cachedUser = Await.result(
+                  BasicAuthenticationDirective
+                    .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
+                  dbOpTimeout)
+                cachedUser.get shouldBe Identity(subject, ns.namespace, ns.authkey, rights = Privilege.ALL)
 
-        stream.toString should include(s"serving from cache: ${CacheKey(ns.authkey)}")
-        stream.reset()
-      }
-    }
+                stream.toString should include(s"serving from cache: ${CacheKey(ns.authkey)}")
+                stream.reset()
+              }
+          }
 
-    // check that invalid keys are rejected
-    val ns = namespaces.head
-    val key = ns.authkey.key.asString
-    Seq(key.drop(1), key.dropRight(1), key + "x", BasicAuthenticationAuthKey().key.asString).foreach { k =>
-      val pass = BasicHttpCredentials(ns.authkey.uuid.asString, k)
-      val user = Await.result(
-        BasicAuthenticationDirective
-          .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
-        dbOpTimeout)
-      user shouldBe empty
-    }
+          // check that invalid keys are rejected
+          val ns = namespaces.head
+          val key = ns.authkey.key.asString
+          Seq(key.drop(1), key.dropRight(1), key + "x", BasicAuthenticationAuthKey().key.asString).foreach { k =>
+            val pass = BasicHttpCredentials(ns.authkey.uuid.asString, k)
+            val user = Await.result(
+              BasicAuthenticationDirective
+                .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
+              dbOpTimeout)
+            user shouldBe empty
+          }
+        },
+        retriesOnTestFailures,
+        Some(waitBeforeRetry),
+        Some(
+          s"${this.getClass.getName} > Authenticate should authorize a known user using different namespaces and cache key, and reject invalid secret not successful, retrying.."))
   }
 
   it should "not log key during validation" in {
-    implicit val tid = transid()
-    val creds = WhiskAuthHelpers.newIdentity()
-    val pass = creds.authkey.getCredentials.asInstanceOf[Option[BasicHttpCredentials]]
-    val user = Await.result(
-      BasicAuthenticationDirective.validateCredentials(pass)(transid, executionContext, logging, authStore),
-      dbOpTimeout)
-    user should be(None)
-    stream.toString should not include pass.get.password
+    org.apache.openwhisk.utils
+      .retry(
+        {
+          implicit val tid = transid()
+          val creds = WhiskAuthHelpers.newIdentity()
+          val pass = creds.authkey.getCredentials.asInstanceOf[Option[BasicHttpCredentials]]
+          val user = Await.result(
+            BasicAuthenticationDirective.validateCredentials(pass)(transid, executionContext, logging, authStore),
+            dbOpTimeout)
+          user should be(None)
+          stream.toString should not include pass.get.password
+        },
+        retriesOnTestFailures,
+        Some(waitBeforeRetry),
+        Some(
+          s"${this.getClass.getName} > Authenticate should not log key during validation not successful, retrying.."))
   }
 
   it should "not authorize an unknown user" in {
-    implicit val tid = transid()
-    val creds = WhiskAuthHelpers.newIdentity()
-    val pass = creds.authkey.getCredentials.asInstanceOf[Option[BasicHttpCredentials]]
-    val user = Await.result(
-      BasicAuthenticationDirective.validateCredentials(pass)(transid, executionContext, logging, authStore),
-      dbOpTimeout)
-    user should be(None)
+    org.apache.openwhisk.utils
+      .retry(
+        {
+          implicit val tid = transid()
+          val creds = WhiskAuthHelpers.newIdentity()
+          val pass = creds.authkey.getCredentials.asInstanceOf[Option[BasicHttpCredentials]]
+          val user = Await.result(
+            BasicAuthenticationDirective.validateCredentials(pass)(transid, executionContext, logging, authStore),
+            dbOpTimeout)
+          user should be(None)
+        },
+        retriesOnTestFailures,
+        Some(waitBeforeRetry),
+        Some(
+          s"${this.getClass.getName} > Authenticate should not authorize an unknown user not successful, retrying.."))
   }
 
   it should "not authorize when no user creds are provided" in {
-    implicit val tid = transid()
-    val user = Await.result(
-      BasicAuthenticationDirective.validateCredentials(None)(transid, executionContext, logging, authStore),
-      dbOpTimeout)
-    user should be(None)
+    org.apache.openwhisk.utils
+      .retry(
+        {
+          implicit val tid = transid()
+          val user = Await.result(
+            BasicAuthenticationDirective.validateCredentials(None)(transid, executionContext, logging, authStore),
+            dbOpTimeout)
+          user should be(None)
+        },
+        retriesOnTestFailures,
+        Some(waitBeforeRetry),
+        Some(
+          s"${this.getClass.getName} > Authenticate should not authorize when no user creds are provided not successful, retrying.."))
   }
 
   it should "not authorize when malformed user is provided" in {
-    implicit val tid = transid()
-    val pass = BasicHttpCredentials("x", Secret().asString)
-    val user = Await.result(
-      BasicAuthenticationDirective
-        .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
-      dbOpTimeout)
-    user should be(None)
+    org.apache.openwhisk.utils
+      .retry(
+        {
+          implicit val tid = transid()
+          val pass = BasicHttpCredentials("x", Secret().asString)
+          val user = Await.result(
+            BasicAuthenticationDirective
+              .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
+            dbOpTimeout)
+          user should be(None)
+        },
+        retriesOnTestFailures,
+        Some(waitBeforeRetry),
+        Some(
+          s"${this.getClass.getName} > Authenticate should not authorize when malformed user is provided not successful, retrying.."))
   }
 
   it should "not authorize when malformed secret is provided" in {
-    implicit val tid = transid()
-    val pass = BasicHttpCredentials(UUID().asString, "x")
-    val user = Await.result(
-      BasicAuthenticationDirective
-        .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
-      dbOpTimeout)
-    user should be(None)
+    org.apache.openwhisk.utils
+      .retry(
+        {
+          implicit val tid = transid()
+          val pass = BasicHttpCredentials(UUID().asString, "x")
+          val user = Await.result(
+            BasicAuthenticationDirective
+              .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
+            dbOpTimeout)
+          user should be(None)
+        },
+        retriesOnTestFailures,
+        Some(waitBeforeRetry),
+        Some(
+          s"${this.getClass.getName} > Authenticate should not authorize when malformed secret is provided not successful, retrying.."))
   }
 
   it should "not authorize when malformed creds are provided" in {
-    implicit val tid = transid()
-    val pass = BasicHttpCredentials("x", "y")
-    val user = Await.result(
-      BasicAuthenticationDirective
-        .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
-      dbOpTimeout)
-    user should be(None)
+    org.apache.openwhisk.utils
+      .retry(
+        {
+          implicit val tid = transid()
+          val pass = BasicHttpCredentials("x", "y")
+          val user = Await.result(
+            BasicAuthenticationDirective
+              .validateCredentials(Some(pass))(transid, executionContext, logging, authStore),
+            dbOpTimeout)
+          user should be(None)
+        },
+        retriesOnTestFailures,
+        Some(waitBeforeRetry),
+        Some(
+          s"${this.getClass.getName} > Authenticate should not authorize when malformed creds are provided not successful, retrying.."))
   }
 }
