134f63e7ff785270a18537db257c3a2021254674
falkzoll <falkzoll@de.ibm.com>
2022-08-03 12:00:44 +0200

    Run unicode tests for newer runtimes. (#110)
    - Also run unicode tests for go:1.15, go:1.17, java, python:3.7, python:3.9 and ruby:2.6 runtimes.

--- tests/dat/actions/build.sh
+++ tests/dat/actions/build.sh
@@ -27,7 +27,9 @@ fi
 jv=$(java -version 2>&1 | head -1 | awk -F'"' '{print $2}')
 if [[ $jv == 1.8.* ]]; then
   echo "java version is $jv (ok)"
-  (cd unicode.tests/src/java/unicode && ../../../../../../../gradlew build && cp build/libs/unicode-1.0.jar ../../../java-8.bin)
+  (cd unicode.tests/src/java/unicode && ../../../../../../../gradlew build && \
+   cp build/libs/unicode-1.0.jar ../../../java-8.bin && \
+   cp build/libs/unicode-1.0.jar ../../../java.bin )
   (cd src/java/sleep && ../../../../../../gradlew build && cp build/libs/sleep-1.0.jar ../../../sleep.jar)
 else
   echo "java version is $jv (not ok)"
